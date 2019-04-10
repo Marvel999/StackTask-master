ec90fb837803b894d3f41ac3b25b21c1a57ad0f7
Marvel999 <manishjobs1999@gmail.com>
2019-04-10 21:30:14 +0530

    Initial commit

---
+++ app/src/main/java/com/Marvel/stackquestion/model/TagResponse.java
@@ -0,0 +1,21 @@
+package com.Marvel.stackquestion.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class TagResponse {
+
+    @SerializedName("items")
+    @Expose
+    private List<TagData> items;
+
+    public List<TagData> getItems() {
+        return items;
+    }
+
+    public void setItems(List<TagData> items) {
+        this.items = items;
+    }
+}
